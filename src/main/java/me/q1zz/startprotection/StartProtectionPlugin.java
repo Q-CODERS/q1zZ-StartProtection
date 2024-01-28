a787d2461c71a8268c0a70198c22881fdbc97a89
q1zZ <q1zZ@zetcraft.pl>
2024-01-28 05:05:10 +0100

    Project & gradle setup

---
+++ src/main/java/me/q1zz/startprotection/StartProtectionPlugin.java
@@ -0,0 +1,17 @@
+package me.q1zz.startprotection;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StartProtectionPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
